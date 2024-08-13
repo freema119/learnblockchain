b2ac888a2e741fd4534b0ff0265c941615278047
FreeMa <hujunroom@qq.com>
2024-08-13 15:14:08 +0800

    Create Counter.sol
    add counter contract

---
+++ w1_1/Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+// 编译器版本声明
+pragma solidity ^0.8.0;
+
+// 合约定义 对标class
+contract Counter{
+    // 状态变量，对标属性
+    uint public counter;
+    constructor(){
+        // 构造函数初始变量
+        counter = 0;
+    }
+    // 计数函数
+    function count() public {
+        counter = counter + 1;
+    }
+}
