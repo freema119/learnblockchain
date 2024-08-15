45686423afc84069085673ad5b8ab2650b54dd9f
FreeMa <hujunroom@qq.com>
2024-08-15 14:10:25 +0800

    创建counter合约

---
+++ w1_hardhat/contracts/Counter.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public counter;
+    constructor() {
+        counter = 0;
+    }
+    function count() public{
+        counter = counter + 1;
+    }
+}
