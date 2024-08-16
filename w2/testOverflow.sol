67edcaf4c9768ee215b1e3c2202985425f585bf9
FreeMa <hujunroom@qq.com>
2024-08-16 13:51:06 +0800

    solidity-类型溢出代码

---
+++ w2/testOverflow.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// pragma solidity ^0.5.0;
+// pragma solidity ^0.8.26;
+
+contract testOverFlow{
+    function add1() public pure returns(uint8){
+        uint8 i = 128;
+        uint8 result = i*2;
+        // 结果值长度大于存储类型，所以现在会直接没法执行，执行报错
+        return result;
+    }
+
+    function add2() public  pure  returns(uint8){
+        uint8 a = 240;
+        uint8 b = 17;
+        uint8 result = a+b;
+        return result;
+    }
+
+    function sub2() public pure returns(uint){
+        uint a = 9;
+        uint b = 10;
+        uint result = a - b;
+        return result;
+    }
+}
